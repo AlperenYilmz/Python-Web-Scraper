5c6e707da05f0283ad38eafb90a785c04b8ec3e2
Alperen <theurgeboat@gmail.com>
2023-03-04 23:21:44 +0300

    comments added

--- href_collector.py
+++ href_collector.py
@@ -6,23 +6,24 @@ headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/
 reqs = requests.get(url, headers=headers)
 soup = BeautifulSoup(reqs.content, 'html.parser')
 
-subpages=[]
-for link in soup.find_all("a"):
+subpages=[]   # creating a list for relative urls
+
+for link in soup.find_all("a"):   # finding links in relative urls
     data=link.get('href')
-    if data not in subpages:
+    if data not in subpages:    # checking for duplications
         subpages.append(data)
     else:
         continue
 
-subpages_copy=[]
+subpages_copy=[]    # creating final list for relative+absolute urls
 
 for sbpg in subpages:
-    if sbpg.startswith('/'):
+    if sbpg.startswith('/'):   # checking if the url is relative url, which starts with "/", else, copy it as it is
         sbpg=url+sbpg
         subpages_copy.append(sbpg)
     else:
         subpages_copy.append(sbpg)
 
-with open(r'C:/Users/alper/Documents/PycharmProjects/CodingAllstars/linkos.txt', 'w') as fp:
+with open(r'C:/Users/alper/Documents/PycharmProjects/CodingAllstars/linkos.txt', 'w') as fp:   # path can be changed as demanded
     for el in subpages_copy:
         fp.write(el+"\n")
